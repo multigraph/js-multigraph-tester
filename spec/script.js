e1724bf0d268c9350f36cde8d04bac2ad8b1bbe1
John Frimmel <jrfrimme@unca.edu>
2012-12-05 15:01:45 -0500

    updates test script to use updated createGraph method

--- spec/script.js
+++ spec/script.js
@@ -6,30 +6,18 @@
             "error" : function (e) {throw e;},
             "warning" : function (e) {throw e;}
         };
-        window.multigraph.parser.jquery.mixin.apply(window.multigraph, "parseXML");
-        window.multigraph.graphics.canvas.mixin.apply(window.multigraph.core);
-        window.multigraph.events.jquery.draggable.mixin.apply(window.multigraph, messageHandler.error);
-        window.multigraph.events.jquery.mouse.mixin.apply(window.multigraph, messageHandler.error);
-        window.multigraph.events.jquery.touch.mixin.apply(window.multigraph, messageHandler.error);
-        window.multigraph.normalizer.mixin.apply(window.multigraph.core);
 
-        var multigraph = window.multigraph.core.Multigraph.parseXML( window.multigraph.parser.jquery.stringToJQueryXMLObj(mugl), messageHandler);
-        multigraph.normalize();
-        multigraph.div(div);
+        var options = {
+            "muglString" : mugl,
+            "div" : div,
+            "driver" : "raphael"
+        }
 
-        $(multigraph.div()).empty();
-
-        window.multigraph.jQuery(div).css('cursor' , 'pointer');
-        multigraph.init();
-        multigraph.registerMouseEvents(multigraph.canvas());
-        multigraph.registerTouchEvents(multigraph.canvas());
-        multigraph.registerCommonDataCallback(function (event) {
-            multigraph.redraw();
-        });
+        $(div).empty();
+        window.multigraph.create(options);
     };
 
     $.fn.jsMultigraphTester = function () {
-        console.log($(this).find("input[type='button']"))
         var button = $(this).find("input[type='button']");
         button.click(refreshButtonHandler);
     }
